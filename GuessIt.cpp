7f3663634b9df075fc74fe1983103c79edc3c531
Something2109 <75402447+Something2109@users.noreply.github.com>
2021-01-31 22:23:54 +0700

    Add files via upload

---
+++ GuessIt.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+#include <cstdlib>
+#include "game.h"
+#include "menu.h"
+#include "score.h"
+using namespace std;
+
+int main() {
+	srand(time(NULL));
+	menu();
+	return 0;
+}
